7fb122aa6f29ddebda2a5b1650590a4ed50cfa62
carlos <carlosacgr@gmail.com>
2020-11-12 22:39:46 -0300

    projeto reajuste salarial exercício 3

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Projeto_Reajuste_Salarial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variáveis
+            decimal salario, salarioReajustado;
+
+            //entrada de dados
+            Console.WriteLine("Digite o Salário atual");
+            salario = decimal.Parse(Console.ReadLine());
+
+            //processamento
+            
+            salarioReajustado = (salario + (30*salario)/100);
+            
+
+            if(salario<500){ 
+                //valor verdadeiro
+                Console.WriteLine("O Salário Reajustado é: "+salarioReajustado+ " Reais ");
+            }
+            else{
+                //valor falso
+                Console.WriteLine("O funcionário não tem direito ao aumento");
+            }
+        }
+    }
+}
